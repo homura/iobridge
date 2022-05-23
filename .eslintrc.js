5626d817fe7c2c654ab0eb5786c6fadd994a82ed
homura <homura.dev@gmail.com>
2022-05-23 18:54:30 +0800

    chore: stricter eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     mocha: true,
   },
   plugins: ['@typescript-eslint'],
-  extends: ['standard', 'plugin:prettier/recommended', 'plugin:node/recommended'],
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   settings: {
     node: {
@@ -17,5 +22,7 @@ module.exports = {
     'node/no-extraneous-import': ['error', { allowModules: ['chai'] }],
     'node/no-unpublished-import': ['error', { allowModules: ['chai'] }],
     'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 };
